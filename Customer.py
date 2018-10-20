1414d322e32902103498858420f86f5c892bc078
Joseph Scavetta <jscavetta95@gmail.com>
2018-10-19 23:03:46 -0400

    Fix style

--- Customer.py
+++ Customer.py
@@ -1,36 +1,33 @@
-'''
+"""
 Created on October 19, 2018
+
 @author: Joseph Scavetta
-'''
+"""
 
 from datetime import datetime
+
+from base import BASE  # Leverage an instance of the declarative_base
 from sqlalchemy import Column, String, PrimaryKeyConstraint, Index
 from sqlalchemy.dialects.mysql import SMALLINT, TIMESTAMP
 from sqlalchemy.orm import relationship
-from base import BASE  # Leverage an instance of the declarative_base
 
 
 class Customer(BASE):
     __tablename__ = 'customer'
 
-    customer_id = Column('customer_id',
-                         SMALLINT(unsigned=True),
-                         nullable=False,
-                         primary_key=True,
-                         autoincrement=True)
-
+    customer_id = Column('customer_id', SMALLINT(unsigned=True), nullable=False, primary_key=True, autoincrement=True)
     first_name = Column('first_name', String(45))
     last_name = Column('last_name', String(45), nullable=False)
     email_address = Column('email_address', String(45), nullable=False)
     last_login = Column('last_login', TIMESTAMP, nullable=False)
     last_update = Column('last_update', TIMESTAMP, nullable=False)
 
-    ratings = relationship("product", secondary="customer_rating", viewonly=True)
-    orders = relationship("product", secondary="customer_order", viewonly=True)
-    shopping_cart = relationship("product", secondary="customer_shopping_cart", viewonly=True)
-    wish_list = relationship("product", secondary="customer_wish_list", viewonly=True)
+    ratings = relationship('product', secondary='customer_rating', viewonly=True)
+    orders = relationship('product', secondary='customer_order', viewonly=True)
+    shopping_cart = relationship('product', secondary='customer_shopping_cart', viewonly=True)
+    wish_list = relationship('product', secondary='customer_wish_list', viewonly=True)
 
-    address = relationship('address"', secondary="customer_address", viewonly=True)
+    address = relationship('address', secondary='customer_address', viewonly=True)
 
     __table_args__ = (
         PrimaryKeyConstraint('customer_id', name='PRIMARY'),
